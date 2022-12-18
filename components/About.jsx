e390854ad9bf97f129dfffb2e14ea052e519c922
yash9650 <yash16022001@gmail.com>
2022-12-18 22:59:02 +0530

    added some responsiveness

--- components/About.jsx
+++ components/About.jsx
@@ -6,11 +6,12 @@ export default function About() {
             <div className={classes.aboutblock}>
                 <h2>About me</h2>
                 <span>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Optio libero perspiciatis asperiores vitae alias ut omnis nihil, iure rem 
-                    laborum hic ex velit illum mollitia eaque possimus harum? Numquam, nemo!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel praesentium velit alias. 
-                    Veniam dolorum sint rerum eum. Dolor totam a sunt, voluptate neque illum placeat ab laborum, provident vero debitis.
+                    <p>
+                    I am a B.tech Computer Science final year student.
+                    I like to build web apps and work on new technologies.
+                    My tech stack contain technologies like React, Node, Express and MongoDB.
+
+                    </p>
                 </span>
             </div>
         </div>
--- components/Project.jsx
+++ components/Project.jsx
@@ -8,30 +8,27 @@ export default function Project() {
                     <h2 style={{ margin: "10px" }}>
                         StopNShop
                     </h2>
-                    <hr />
-                    <article style={{ margin: "10px" }}>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum rerum consectetur incidunt, doloribus dolor tenetur iusto magni placeat inventore nihil enim sit amet ut itaque harum ad veritatis necessitatibus beatae?
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis, ex magni omnis aliquam sequi placeat voluptates eligendi quod inventore eos nam, sed itaque ipsa ut molestiae velit deserunt. Alias, amet?
+                    {/* <hr /> */}
+                    <article className={classes.article} style={{ margin: "10px" }}>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum rerum consectetur incidunt, doloribus dolor tenetur iusto magni placeat.
                     </article>
                 </span>
                 <span className={classes.project}>
                     <h2 style={{ margin: "10px" }}>
                         First-step
                     </h2>
-                    <hr />
-                    <article style={{ margin: "10px" }}>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum rerum consectetur incidunt, doloribus dolor tenetur iusto magni placeat inventore nihil enim sit amet ut itaque harum ad veritatis necessitatibus beatae?
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis, ex magni omnis aliquam sequi placeat voluptates eligendi quod inventore eos nam, sed itaque ipsa ut molestiae velit deserunt. Alias, amet?
+                    {/* <hr /> */}
+                    <article className={classes.article} style={{ margin: "10px" }}>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum rerum consectetur incidunt, doloribus dolor tenetur iusto magni placeat.
                     </article>
                 </span>
                 <span className={classes.project}>
                     <h2 style={{ margin: "10px" }}>
                         Chat-App
                     </h2>
-                    <hr />
-                    <article style={{ margin: "10px" }}>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum rerum consectetur incidunt, doloribus dolor tenetur iusto magni placeat inventore nihil enim sit amet ut itaque harum ad veritatis necessitatibus beatae?
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis, ex magni omnis aliquam sequi placeat voluptates eligendi quod inventore eos nam, sed itaque ipsa ut molestiae velit deserunt. Alias, amet?
+                    {/* <hr /> */}
+                    <article className={classes.article} style={{ margin: "10px" }}>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum rerum consectetur incidunt, doloribus dolor tenetur iusto magni placeat.
                     </article>
                 </span>
             </div>
