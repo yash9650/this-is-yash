ad4ef9f9456679dd71561683f86ebaee778cb7f7
yash9650 <yash16022001@gmail.com>
2022-12-20 21:35:45 +0530

    project-page responsiveness

--- components/Project.jsx
+++ components/Project.jsx
@@ -1,37 +1,55 @@
-import classes from './styles/Project.module.scss';
+import classes from "./styles/Project.module.scss";
 
 export default function Project() {
-    return (
-        <div className={classes.projectcontainer}>
-            <div className={classes.projectblock}>
-                <span className={classes.project}>
-                    <h2 style={{ margin: "10px" }}>
-                        StopNShop
-                    </h2>
-                    {/* <hr /> */}
-                    <article className={classes.article} style={{ margin: "10px" }}>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum rerum consectetur incidunt, doloribus dolor tenetur iusto magni placeat.
-                    </article>
-                </span>
-                <span className={classes.project}>
-                    <h2 style={{ margin: "10px" }}>
-                        First-step
-                    </h2>
-                    {/* <hr /> */}
-                    <article className={classes.article} style={{ margin: "10px" }}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum rerum consectetur incidunt, doloribus dolor tenetur iusto magni placeat.
-                    </article>
-                </span>
-                <span className={classes.project}>
-                    <h2 style={{ margin: "10px" }}>
-                        Chat-App
-                    </h2>
-                    {/* <hr /> */}
-                    <article className={classes.article} style={{ margin: "10px" }}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum rerum consectetur incidunt, doloribus dolor tenetur iusto magni placeat.
-                    </article>
-                </span>
-            </div>
-        </div>
-    );
+  return (
+    <div className={classes.projectcontainer}>
+      <div className={classes.projectblock}>
+        <a
+          className={classes.project}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.github.com/yash9650/StopNShop"
+        >
+          <h2 style={{ margin: "10px" }}>StopNShop</h2>
+          {/* <hr /> */}
+          <article className={classes.article} style={{ margin: "10px" }}>
+            StopNShp is a campus website, the idea was to provide shops in our
+            campus online. StopNShop contains features like, Login/Register, Add
+            items to card, Order products and many more.
+          </article>
+        </a>
+
+        <a
+          className={classes.project}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.github.com/yash9650/first-step"
+        >
+          <h2 style={{ margin: "10px" }}>First-step</h2>
+          {/* <hr /> */}
+          <article className={classes.article} style={{ margin: "10px" }}>
+            FristStep is a medical website. The idea is to provide a platform
+            where a user can search disease by disease name or its symptoms. Our
+            website will provide home Remedies and other details about the
+            disease.
+          </article>
+        </a>
+        <a
+          className={classes.project}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.github.com/yash9650/chatapp"
+        >
+          <h2 style={{ margin: "10px" }}>Chat-App</h2>
+          {/* <hr /> */}
+          <article className={classes.article} style={{ margin: "10px" }}>
+            ChatApp allows the student to intract with new people/student in
+            their campus. The idea was to connect students in the same
+            university or different university with each other so they can study
+            together and help each other at the time of examinations.
+          </article>
+        </a>
+      </div>
+    </div>
+  );
 }
